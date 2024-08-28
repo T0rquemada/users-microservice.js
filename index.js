f3f5a4a8da1a232b757279b6309ee88b3191faac
T0rquemada <viktor.debian@gmail.com>
2024-08-28 15:36:21 +0300

    Allow all origins. Remove env for tests

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const UsersRouter = require('./server/Router.js');
 
 const corsOptions = {
-    origin: 'http://127.0.0.1:5500',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
 };
@@ -17,10 +17,8 @@ app.use(express.json());
 
 app.use('/users', UsersRouter);
 
-if (process.env.NODE_ENV !== 'test') {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
